00136ef27c28cc20e54d2b33cc8c272391b98545
KonnyAlucema <lisethtorres969@gmail.com>
2023-10-06 14:00:47 -0500

    feat:

--- BackEnd/Infrastructure/Repository/EspecieRepository.cs
+++ BackEnd/Infrastructure/Repository/EspecieRepository.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Infrastructure.Repository
-{
+namespace Infrastructure.Repository;
     public class EspecieRepository
     {
         
     }
-}
